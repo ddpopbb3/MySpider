6ed5117116fecb72311f27f8762272375f41bc13
Lyp <Lyp@Lyp-PC>
2017-08-21 12:59:01 +0800

    update : 更新了pom文件，更改了main函数传参的方式

--- src/main/java/com/spider/demo/lyp/main/Main.java
+++ src/main/java/com/spider/demo/lyp/main/Main.java
@@ -2,12 +2,14 @@ package com.spider.demo.lyp.main;
 
 import com.spider.demo.lyp.MyFirstSpider.CSDNblogCrawler;
 
+/**
+ * 主程序入口，参数为下载地址
+ * */
 public class Main {
 	public static void main(String[] args) {
-		// 通过main函数传参
-		String dir = args[0];
-		// 下载热门博客前20页的内容
-		for (int i = 1; i <= 20; i++) {
+		String dir = "E:\\CSDNblog\\";
+	
+		for (int i = 1; i <= 20; i++) {  // 下载热门博客前20页的内容
 			try {
 				new CSDNblogCrawler(i,dir).startCopy();
 			} catch (InterruptedException e) {
